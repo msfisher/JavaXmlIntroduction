27146667c40e53744a1f35769e820593b9cb5751
msfisher <Marcus.S.Fisher@gmail.com>
2016-06-05 14:57:17 -0400

    first commit: simple example project processing XML

---
+++ src/xmlintroduction/XmlIntroduction.java
@@ -0,0 +1,14 @@
+package xmlintroduction;
+
+//-----------------------------------------------------------------------------
+//Parsing XML refers to going through XML document to access data or modify
+//data in some way
+
+
+public class XmlIntroduction {
+
+    public static void main(String[] args) {
+        //Entering main() method
+    }//end of main() method
+    
+}//end of class XmlIntroduction
